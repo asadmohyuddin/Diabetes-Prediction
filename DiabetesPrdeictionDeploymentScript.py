9c15e194274302526ee1989ef9d73f941ecc5193
Syed Asad Mohyuddin <asad.mohyuddin15@gmail.com>
2025-01-21 23:52:04 +0500

    Update DiabetesPrdeictionDeploymentScript.py

--- DiabetesPrdeictionDeploymentScript.py
+++ DiabetesPrdeictionDeploymentScript.py
@@ -54,6 +54,8 @@ def main():
     
     if st.button('Diabetes Test Result'):
         diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+        if(diagnosis == '0')
         st.balloons()
 
 
