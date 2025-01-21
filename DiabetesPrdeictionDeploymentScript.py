1e1893eb37b4c83d2d0ac8f8b6335c7bccdc4d24
Syed Asad Mohyuddin <asad.mohyuddin15@gmail.com>
2025-01-21 23:41:04 +0500

    Update DiabetesPrdeictionDeploymentScript.py

--- DiabetesPrdeictionDeploymentScript.py
+++ DiabetesPrdeictionDeploymentScript.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # loading the saved model
-loaded_model = pickle.load(open("C:/Users/DELL/Documents/Machine Learning Projects/Diabetes Prediction/diabetes_model.sav", 'rb'))
+loaded_model = pickle.load(open("diabetes_model.sav", 'rb'))
 
 
 # creating a function for Prediction
